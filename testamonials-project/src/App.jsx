90e1bbe036e551490c7d0b7d7f5520e2faed4937
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-23 17:46:07 +0200

    Setting up project files

---
+++ testamonials-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Testamonials from './components/Testamonials'
+
+const App = () => {
+  return (
+    <div>
+      <Testamonials/>
+    </div>
+  )
+}
+
+export default App
