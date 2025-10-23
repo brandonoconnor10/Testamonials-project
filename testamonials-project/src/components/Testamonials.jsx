848edef9591805d020e9b1b9198b809f100425b0
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-23 17:47:59 +0200

    React code for the testimonial project

--- testamonials-project/src/components/Testamonials.jsx
+++ testamonials-project/src/components/Testamonials.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles.css";
 
-const Testamonials = () => {
+function Testamonials() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const testamonials = [
+    {
+      quote: "This is the best product I've ever used!",
+      author: "Jane Doe",
+    },
+    {
+      quote: "I highly recommend this product to everyone!",
+      author: "John Smith",
+    },
+    {
+      quote: "This product has completely changed my life!",
+      author: "Bob Johnson",
+    },
+  ];
+
+  const handlePrevClick = () => {
+    setCurrentIndex(
+      (currentIndex + testamonials.length - 1) % testamonials.length
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % testamonials.length);
+  };
+
   return (
-    <div>Testamonials</div>
-  )
+    <div className="testamonials">
+      <div className="testamonials-quote">
+        {testamonials[currentIndex].quote}
+      </div>
+      <div className="testamonials-author">
+        - {testamonials[currentIndex].author}
+      </div>
+      <div className="testamonials-nav">
+        <button onClick={handlePrevClick}>Prev</button>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
+    </div>
+  );
 }
 
-export default Testamonials
+export default Testamonials;
